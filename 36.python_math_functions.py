7376ecdc51d413f5790b0eac95ce0b3109b13ec0
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-27 08:23:02 -0900

    Add files via upload

---
+++ 36.python_math_functions.py
@@ -0,0 +1,34 @@
+a=min(10,20,30,40,50)
+print(f"Min is: ",a)
+
+b=max(10,20,30,40,50)
+print(f"Max is: ",b)
+
+c=35.4
+d=35.5
+print(f"Round off {c} : ",round(c))
+print(f"Round off {d} : ",round(d))
+
+import math
+e=2.3
+print(f"Ceil of {e} is : ",math.ceil(e))
+print(f"Floor of {e} is: ",math.floor(e))
+
+f=5
+print(f"Factorial of {f} is: ",math.factorial(f))
+
+g=15
+h=5
+print(f"GCD of {15} and {h} is: ",math.gcd(g,h))
+
+i=3
+j=2
+print(f"Power of {i}**{j} is: ",pow(i,j))
+
+k=2  #log
+l=3   #base
+print(f"log of {k} to the base {l} is: ",math.log(k,l))
+
+m=64
+print(f"Square root of {m} is: ",math.sqrt(m))
+
